5440ac2120057c4309f68f9b6a4ac43a06e9e549
Matthew <matthewjo30@gmail.com>
2020-10-10 13:38:45 -0400

    Updates

---
+++ precache-manifest.23a0144e01fa7a53524a6dc3e464c16f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3eaf6edc6b2fafa5af43a8ed54ed30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c47d011995522d9fe668",
+    "url": "/static/css/main.3f1ab027.chunk.css"
+  },
+  {
+    "revision": "63a03a93312d57430dfe",
+    "url": "/static/js/2.c53ec7d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c53ec7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47d011995522d9fe668",
+    "url": "/static/js/main.7e6353c7.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  },
+  {
+    "revision": "c761af6278d99063591ffb757d6b94be",
+    "url": "/static/media/carleton.c761af62.png"
+  },
+  {
+    "revision": "7cf3a1f285039c48dd2c3991c2f9a869",
+    "url": "/static/media/concentration.7cf3a1f2.png"
+  },
+  {
+    "revision": "e893dea4685be2ba2e8d382ab514314b",
+    "url": "/static/media/feenics.e893dea4.png"
+  },
+  {
+    "revision": "704a643d40fafd1e65d5f552a993e092",
+    "url": "/static/media/githubIcon.704a643d.png"
+  },
+  {
+    "revision": "01b237ac1abbd3223d1ff35ed0ff9759",
+    "url": "/static/media/me.01b237ac.png"
+  },
+  {
+    "revision": "0a32f4485a6cb5e75aa95429b03e306e",
+    "url": "/static/media/towerdefense.0a32f448.png"
+  },
+  {
+    "revision": "c2c63bbbf64c678b3df705b3891ce259",
+    "url": "/static/media/trivia.c2c63bbb.png"
+  }
+]);
